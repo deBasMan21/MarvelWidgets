cd9972f9596210f103a37d578eaaafbcbd09ed31
deBasMan21 <bbuijsen@gmail.com>
2022-08-10 14:10:38 +0200

    Initial Commit

---
+++ MarvelWidgets/MarvelWidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelWidgetsApp.swift
+//  MarvelWidgets
+//
+//  Created by Bas Buijsen on 10/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct MarvelWidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
